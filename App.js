788deb24043394b643b59a92af550dc75e2db211
sangmokchae <72491390+SangMokChae@users.noreply.github.com>
2021-02-01 23:55:08 +0900

    on Android

--- App.js
+++ App.js
@@ -3,13 +3,28 @@ import React from 'react';
 import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
+import axios from "axios";
+import Weather from "./Weather";
+
+const API_KEY = "b62020883b35c55f405ea3737ccc87bc";
 
 export default class extends React.Component {
+  state = {
+    isLoading: true
+  };
+  getWeather = async() => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
+    );
+    this.setState({ isLoading: false, temp: data.main.temp });
+  };
   getLocation = async() => {
     try {
       await Location.requestPermissionsAsync();
-      const location = await Location.getCurrentPositionAsync();
-      console.log(location);
+      const {coords: { latitude, longitude }
+    } = await Location.getCurrentPositionAsync();
+    this.getWeather(latitude, longitude);
+
     } catch (error) {
       Alert.alert("Can't find you.", "So sad");
     }
@@ -18,6 +33,7 @@ export default class extends React.Component {
     this.getLocation();
   }
   render() {
-    return <Loading />;
+    const { isLoading, temp } = this.state;
+    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} />;
   }
 }
---
+++ Weather.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import PropTypes from "prop-types";
+
+
+export default function Weather() {
+    return (
+        <View style={styles.container}>
+            <Text>{temp}</Text>
+        </View>
+    );
+}
+
+Weather.PropTypes = {
+    temp: PropTypes.number.isRequired
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    }
+})
